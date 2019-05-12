31e0f5bae3e942a76a0c9514cca1d80ebeafa584
Atif Aslam <aslam43ecehstu@gmail.com>
2019-05-13 00:11:11 +0600

    update completed

--- HSTU_SCHOOL/HSTU_SCHOOL/HSTU_SCHOOL/settings.py
+++ HSTU_SCHOOL/HSTU_SCHOOL/HSTU_SCHOOL/settings.py
@@ -17,6 +17,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'hstu_school',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -40,7 +41,9 @@ ROOT_URLCONF = 'HSTU_SCHOOL.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            os.path.join(BASE_DIR,'templates')
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -61,11 +64,14 @@ WSGI_APPLICATION = 'HSTU_SCHOOL.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hstu_school',
+        'USER': 'aslam43',
+        'PASSWORD': 'aslam43',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
@@ -104,3 +110,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[
+    os.path.join(BASE_DIR,'static')
+]
+
