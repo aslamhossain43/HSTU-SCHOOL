2d288a71344dd024a63d2dee7bac2341c1619618
Atif Aslam <aslam43ecehstu@gmail.com>
2019-05-17 02:02:24 +0600

    update completed

---
+++ HSTU_SCHOOL/HSTU_SCHOOL/static/customjs/teacher.js
@@ -0,0 +1,9 @@
+// for teacher list
+$(document).ready(function() {
+    $('#teacherTable').DataTable();
+} );
+// manage teacher list
+$(document).ready(function() {
+    $('#manageTeacherTable').DataTable();
+} );
+
