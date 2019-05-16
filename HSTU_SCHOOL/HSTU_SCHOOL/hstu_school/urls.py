591f36202b6d675658e0c477809abdd08e166fc0
Atif Aslam <aslam43ecehstu@gmail.com>
2019-05-16 22:47:42 +0600

    update completed

--- HSTU_SCHOOL/HSTU_SCHOOL/hstu_school/urls.py
+++ HSTU_SCHOOL/HSTU_SCHOOL/hstu_school/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 
 from . import views
@@ -12,8 +14,13 @@ urlpatterns = [
     path('event/', views.event,name='event'),
     path('news/', views.news,name='news'),
     path('manage/', views.manage,name='manage'),
-
-
+    path('addTeacherList/',views.addTeacherList,name='addTeacherList'),
+    path('editTeacher/<id>',views.requestForUpdateTeacherList,name='requestForUpdateTeacherList'),
+    path('updateTeacherList/<id>',views.updateTeacherList,name='updateTeacher'),
+    path('deleteTeacher/<id>',views.deleteTeacher,name='deleteTeacher'),
 
 
 ]
+
+if settings.DEBUG: # remember to set 'DEBUG = True' in settings.py
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
